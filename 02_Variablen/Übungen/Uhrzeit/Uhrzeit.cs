db6cb8c778755999eb35a3746171a33d18c9948f
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-10-12 13:40:46 +0200

    Tipo in grow => grown

--- 02_Variablen/Übungen/Uhrzeit/Uhrzeit.cs
+++ 02_Variablen/Übungen/Uhrzeit/Uhrzeit.cs
@@ -15,22 +15,21 @@ Console.WriteLine("=============================");
 Console.WriteLine();
 
 Console.Write("Please enter amount of seconds [0..86399]:");
-var input = Console.ReadLine();
-var totalSeconds = Convert.ToInt32(input); 
+var input        = Console.ReadLine();
+var totalSeconds = Convert.ToInt32(input);
 
-if (totalSeconds < (24*60*60))
+if (totalSeconds < (24 * 60 * 60))
 {
-	// Ausgabeformat: hh:mm:ss
+    // Ausgabeformat: hh:mm:ss
 
-	var seconds = totalSeconds % 60;
-	var totalMinutes = totalSeconds / 60;
-	var minutes = totalMinutes % 60;
-	var hours = totalMinutes / 60;
+    var seconds      = totalSeconds % 60;
+    var totalMinutes = totalSeconds / 60;
+    var minutes      = totalMinutes % 60;
+    var hours        = totalMinutes / 60;
 
-	Console.WriteLine($"{hours:00}:{minutes:00}:{seconds:00}"); 	
+    Console.WriteLine($"{hours:00}:{minutes:00}:{seconds:00}");
 }
 else
 {
-	Console.WriteLine($"Falsche Eingabe: {totalSeconds}");
+    Console.WriteLine($"Falsche Eingabe: {totalSeconds}");
 }
-
