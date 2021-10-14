e103a3ce7f0ab3836a947a4a42854f3145e6a87b
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-10-14 17:14:16 +0200

    DayOfYear

---
+++ 03_Selektion/Übungen/DayOfYear/DayOfYearWithoutLeapYear_Switch.cs
@@ -0,0 +1,101 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *                Musterlösung - HA
+ *--------------------------------------------------------------
+ * Description:
+ * Calculation of "DayOfYear" (without leap year)
+ *--------------------------------------------------------------
+*/
+
+using System;
+
+Console.WriteLine("Day of year calculation");
+Console.WriteLine("-----------------------");
+
+Console.Write("Please enter month [1..12]");
+var month = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter day [1..31]");
+var day = Convert.ToInt32(Console.ReadLine());
+
+int daysInMonth;
+
+switch (month)
+{
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        daysInMonth = 31;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        daysInMonth = 30;
+        break;
+    case 2:
+        daysInMonth = 28;
+        break;
+    default:
+        daysInMonth = 0;
+        Console.WriteLine($"Invalid month {month}");
+        break;
+}
+
+if (daysInMonth > 0)
+{
+    if (day >= 1 && day <= daysInMonth)
+    {
+        var dayOfYear = day;
+
+        switch (month)
+        {
+            default:
+                break;
+            case 2:
+                dayOfYear += 31;
+                break;
+            case 3:
+                dayOfYear += 31 + 28;
+                break;
+            case 4:
+                dayOfYear += 31 + 28 + 31;
+                break;
+            case 5:
+                dayOfYear += 31 + 28 + 31 + 30;
+                break;
+            case 6:
+                dayOfYear += 31 + 28 + 31 + 30 + 31;
+                break;
+            case 7:
+                dayOfYear += 31 + 28 + 31 + 30 + 31 + 30;
+                break;
+            case 8:
+                dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31;
+                break;
+            case 9:
+                dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+                break;
+            case 10:
+                dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+                break;
+            case 11:
+                dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+                break;
+            case 12:
+                dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+                break;
+        }
+
+        Console.WriteLine($"Day of the year {month}:{day} = {dayOfYear}");
+    }
+    else
+    {
+        Console.WriteLine($"Invalid day: in month {month} should be: 1 <= {day} <= {daysInMonth}");
+    }
+}
---
+++ 03_Selektion/Übungen/DayOfYear/DayOfYearWithoutLeapYear_elseIf.cs
@@ -0,0 +1,99 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *                Musterlösung - HA
+ *--------------------------------------------------------------
+ * Description:
+ * Calculation of "DayOfYear" (without leap year)
+ *--------------------------------------------------------------
+*/
+
+using System;
+
+Console.WriteLine("Day of year calculation");
+Console.WriteLine("-----------------------");
+
+Console.Write("Please enter month [1..12]");
+var month = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter day [1..31]");
+var day = Convert.ToInt32(Console.ReadLine());
+
+int daysInMonth;
+
+if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+{
+    daysInMonth = 31;
+}
+else if (month == 4 || month == 6 || month == 9 || month == 11)
+{
+    daysInMonth = 30;
+}
+else if (month == 2)
+{
+    daysInMonth = 28;
+}
+else
+{
+    daysInMonth = 0;
+    Console.WriteLine($"Invalid month {month}");
+}
+
+if (daysInMonth > 0)
+{
+    if (day >= 1 && day <= daysInMonth)
+    {
+        var dayOfYear = day;
+
+        if (month == 2)
+        {
+            dayOfYear += 31;
+        }
+        else if (month == 3)
+        {
+            dayOfYear += 31 + 28;
+        }
+        else if (month == 4)
+        {
+            dayOfYear += 31 + 28 + 31;
+        }
+        else if (month == 5)
+        {
+            dayOfYear += 31 + 28 + 31 + 30;
+        }
+        else if (month == 6)
+        {
+            dayOfYear += 31 + 28 + 31 + 30 + 31;
+        }
+        else if (month == 7)
+        {
+            dayOfYear += 31 + 28 + 31 + 30 + 31 + 30;
+        }
+        else if (month == 8)
+        {
+            dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31;
+        }
+        else if (month == 9)
+        {
+            dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+        }
+        else if (month == 10)
+        {
+            dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+        }
+        else if (month == 11)
+        {
+            dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+        }
+        else if (month == 12)
+        {
+            dayOfYear += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+        }
+
+        Console.WriteLine($"Day of the year {month}:{day} = {dayOfYear}");
+    }
+    else
+    {
+        Console.WriteLine($"Invalid day: in month {month} should be: 1 <= {day} <= {daysInMonth}");
+    }
+}
