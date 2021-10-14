9c8c1b418a13b5fb31303ce88af0dcf8c7f2b475
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-10-14 15:38:22 +0200

    LZK

---
+++ 02_Variablen/Übungen/Visitenkarte/Visitenkarte.cs
@@ -0,0 +1,28 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description:
+* Ausgabe Visitenkarte
+*--------------------------------------------------------------
+*/
+
+using System;
+
+// Eingabe
+Console.WriteLine("Visitenkarte");
+Console.WriteLine("=====================================");
+Console.WriteLine();
+
+Console.Write("Bitte geben sie ihren Vornamen ein: ");
+var vorname = Console.ReadLine();
+
+Console.Write("Bitte geben sie ihren Nachnamen ein: ");
+var nachname = Console.ReadLine();
+
+// Ausgabe
+Console.WriteLine("+-----------------+");
+Console.WriteLine($"| {vorname,-15} |");
+Console.WriteLine($"| {nachname,-15} |");
+Console.WriteLine("+-----------------+");
