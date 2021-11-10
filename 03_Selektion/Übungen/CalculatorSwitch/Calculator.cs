f758b144fc41dea9b70a1520d3a7ef6a658d9af4
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-11-10 08:37:17 +0100

    Calculator (switch/loop)

---
+++ 03_Selektion/Übungen/CalculatorSwitch/Calculator.cs
@@ -0,0 +1,60 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Beschreibung:
+* Ein einfacher Rechner, der die vier Grundrechnungsarten beherscht.
+* Für die Auswahl der Operation verwenden wir ein switch.
+*--------------------------------------------------------------
+*/
+
+using System;
+
+Console.WriteLine("Calculator with Switch");
+Console.WriteLine("====================");
+Console.WriteLine();
+
+Console.Write("Please enter left operand [double]: ");
+var leftOperand = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter operator: [+,-,*,/]: ");
+var operatorInput = Console.ReadLine();
+
+Console.Write("Please enter right operand [double]: ");
+var rightOperand = Convert.ToDouble(Console.ReadLine());
+
+switch (operatorInput)
+{
+    case "+":
+        var plus = leftOperand + rightOperand;
+        Console.WriteLine($"{leftOperand} + {rightOperand} = {plus}");
+        break;
+
+    case "-":
+        var minus = leftOperand - rightOperand;
+        Console.WriteLine($"{leftOperand} - {rightOperand} = {minus}");
+        break;
+
+    case "*":
+        var mult = leftOperand * rightOperand;
+        Console.WriteLine($"{leftOperand} * {rightOperand} = {mult}");
+        break;
+
+    case "/":
+        if (rightOperand == 0.0)
+        {
+            Console.WriteLine("Division by 0.0!");
+        }
+        else
+        {
+            var div = leftOperand / rightOperand;
+            Console.WriteLine($"{leftOperand} / {rightOperand} = {div}");
+        }
+
+        break;
+
+    default:
+        Console.WriteLine($"Illegal operator {operatorInput}!");
+        break;
+}
---
+++ 05_Schleifen/Übungen/01_while_do-while/CalculatorLoop/Calculator.cs
@@ -0,0 +1,65 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Beschreibung:
+* Ein einfacher Rechner, der die vier Grundrechnungsarten beherscht.
+* Es wird solange gerechnet bis der Benutzer X eingibt.
+*--------------------------------------------------------------
+*/
+
+using System;
+
+Console.WriteLine("Continues Calculator");
+Console.WriteLine("====================");
+Console.WriteLine();
+
+Console.Write("Please enter operator: [+,-,*,/,X for exit]: ");
+var operatorInput = Console.ReadLine();
+
+while (operatorInput != "X")
+{
+    Console.Write("Please enter left operand [double]: ");
+    var leftOperand = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Please enter right operand [double]: ");
+    var rightOperand = Convert.ToDouble(Console.ReadLine());
+
+    switch (operatorInput)
+    {
+        case "+":
+            var plus = leftOperand + rightOperand;
+            Console.WriteLine($"{leftOperand} + {rightOperand} = {plus}");
+            break;
+
+        case "-":
+            var minus = leftOperand - rightOperand;
+            Console.WriteLine($"{leftOperand} - {rightOperand} = {minus}");
+            break;
+
+        case "*":
+            var mult = leftOperand * rightOperand;
+            Console.WriteLine($"{leftOperand} * {rightOperand} = {mult}");
+            break;
+
+        case "/":
+            if (rightOperand == 0.0)
+            {
+                Console.WriteLine("Division by 0.0!");
+            }
+            else
+            {
+                var div = leftOperand / rightOperand;
+                Console.WriteLine($"{leftOperand} / {rightOperand} = {div}");
+            }
+            break;
+
+        default:
+            Console.WriteLine($"Illegal operator {operatorInput}!");
+            break;
+    }
+
+    Console.Write("Please enter operator: [+,-,*,/,X for exit]: ");
+    operatorInput = Console.ReadLine();
+}
