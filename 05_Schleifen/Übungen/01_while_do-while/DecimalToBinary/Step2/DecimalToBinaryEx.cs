c7b17e8d66918b51a430d5db87790ee88e0e5dea
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-11-17 08:25:53 +0100

    DecimalToBinary

---
+++ 05_Schleifen/Übungen/01_while_do-while/DecimalToBinary/Step2/DecimalToBinaryEx.cs
@@ -0,0 +1,52 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+ * Beschreibung:
+ * Das Programm wandelt eine positve Ganzzahl aus dem Dezimalsystem
+ * in das Binärsystem um
+*--------------------------------------------------------------
+*/
+
+using System;
+
+Console.Write("Please enter a decimal value [0..] (-1 for exit): ");
+var decimalValue = Convert.ToInt32(Console.ReadLine());
+
+while (decimalValue != -1)
+{
+    var remaining = decimalValue;
+    var binaryString = "";
+
+    if (decimalValue == 0)
+    {
+        binaryString = "0";
+    }
+    else
+    {
+        if (remaining < 0)
+        {
+            remaining = -remaining;
+        }
+
+        while (remaining != 0)
+        {
+            var rem = remaining % 2;
+            remaining = remaining / 2;
+
+            binaryString = rem + binaryString;
+            // Console.WriteLine($"remaining: {remaining}, Rest: {rem}");
+        }
+    }
+
+    if (decimalValue < 0)
+    {
+        binaryString = "-" + binaryString;
+    }
+
+    Console.WriteLine($"Decimal: {decimalValue} => {binaryString}");
+
+    Console.Write("Please enter a decimal value [0..] (-1 for exit): ");
+    decimalValue = Convert.ToInt32(Console.ReadLine());
+}
