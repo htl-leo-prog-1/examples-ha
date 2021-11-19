306fe90af2320e3948e5ef6f616a60495850b2ae
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-11-19 19:08:18 +0100

    Move and corr

---
+++ 05_Schleifen/Leistungsfeststellungen/2021/LF1Template/ParckingCharge/ParkingCharge.cs
@@ -0,0 +1,12 @@
+/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: <CLASSNAME>
+ *--------------------------------------------------------------
+ *              <NAME> 
+ *--------------------------------------------------------------
+ * Description: ParkingCharge - Berechnung der Parkgebühr
+ *--------------------------------------------------------------
+*/
+
+using System;
+
+Aktualisieren Sie ihren Namen im BlockHeader
