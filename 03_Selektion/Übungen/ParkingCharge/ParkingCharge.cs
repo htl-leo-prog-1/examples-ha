1ed681e9c403aa4d8e9829e8ed27d7ecb8a46976
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-11-19 18:34:04 +0100

    ParkingCharge

---
+++ 03_Selektion/Übungen/ParkingCharge/ParkingCharge.cs
@@ -0,0 +1,64 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description:
+*--------------------------------------------------------------
+*/
+
+using System;
+
+Console.WriteLine("Calculate parking charge");
+Console.WriteLine("========================");
+Console.WriteLine();
+
+const double MINCHARGE           = 2.0;
+const double HOURSCHARGEFIRST    = 5.0;
+const double CHARGEPERHOUREFIRST = 1;
+const double CHARGEPERHOUREAFTER = 0.5;
+const double MAXCHARGE           = 10.0;
+
+Console.Write("Please enter hours: ");
+var parkingHours = Convert.ToDouble(Console.ReadLine());
+
+while (parkingHours > 0)
+{
+    var charge = 0.0;
+
+    parkingHours = Math.Ceiling(parkingHours);
+
+    if (parkingHours > 24)
+    {
+        var days = (int)parkingHours / 24;
+        charge       =  days * MAXCHARGE;
+        parkingHours -= days * 24;
+    }
+
+    var dayCharge = 0.0;
+
+    if (parkingHours >= HOURSCHARGEFIRST)
+    {
+        dayCharge    = (parkingHours - HOURSCHARGEFIRST) * CHARGEPERHOUREAFTER;
+        parkingHours = HOURSCHARGEFIRST;
+    }
+
+    dayCharge += parkingHours * CHARGEPERHOUREFIRST;
+
+    if (dayCharge > MAXCHARGE)
+    {
+        dayCharge = MAXCHARGE;
+    }
+
+    if (dayCharge < MINCHARGE)
+    {
+        dayCharge = MINCHARGE;
+    }
+
+    charge += dayCharge;
+
+    Console.WriteLine($"You have to pay {charge:F2} Euro");
+
+    Console.Write("Please enter hours: (0 to exit): ");
+    parkingHours = Convert.ToDouble(Console.ReadLine());
+}
