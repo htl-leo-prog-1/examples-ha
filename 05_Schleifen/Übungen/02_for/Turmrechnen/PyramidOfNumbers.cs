973d61d5132d2fee17c092c37d1bf52c4123b1b0
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-11-26 15:58:56 +0100

    Turmrechnen

---
+++ 05_Schleifen/Übungen/02_for/Turmrechnen/PyramidOfNumbers.cs
@@ -0,0 +1,41 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: PyramidOfNumbers
+*--------------------------------------------------------------
+*/
+
+using System;
+
+ulong currentBase;
+
+do
+{
+    bool isOk;
+
+    do
+    {
+        Console.Write("Please enter a number [0..] (0 for exit): ");
+        isOk = ulong.TryParse(Console.ReadLine(), out currentBase);
+    } while (!isOk);
+
+    if (currentBase != 0)
+    {
+        ulong lastResult = currentBase;
+        for (ulong i = 2; i <= 10; i++)
+        {
+            currentBase = lastResult;
+            lastResult  = currentBase * i;
+            Console.WriteLine($"{currentBase} * {i} = {lastResult}");
+        }
+
+        for (ulong i = 2; i <= 10; i++)
+        {
+            currentBase = lastResult;
+            lastResult  = currentBase / i;
+            Console.WriteLine($"{currentBase} / {i} = {lastResult}");
+        }
+    }
+} while (currentBase != 0);
