65373736e8e91c95023dc4b1b1c862c4596f95e9
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2021-12-21 15:50:01 +0100

    Mirp + Chess3D

---
+++ 05_Schleifen/Übungen/04_stringLoops/Chess3D/Chess3D.cs
@@ -0,0 +1,90 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: Chess 3D
+*--------------------------------------------------------------
+*/
+
+using System;
+
+Console.WriteLine("Chess 3D");
+Console.WriteLine("**************");
+
+const int SIZE = 8;
+const int TOTALLINES = SIZE + 3;
+const int TOTALDIFF = SIZE + 2;
+
+int shift;
+bool isOk;
+
+do
+{
+    do
+    {
+        Console.Write("Please enter horizontal shift: ");
+        isOk = int.TryParse(Console.ReadLine(), out shift);
+    } while (!isOk || shift < 0);
+
+    int currentLine = TOTALLINES-1;
+    int CurrentShift(int line) => (line * shift + TOTALDIFF / 2) / TOTALDIFF;
+
+    for (var x = 0; x < CurrentShift(currentLine); x++)
+    {
+        Console.Write(" ");
+    }
+
+    Console.Write("+");
+    for (var x = 0; x < SIZE; x++)
+    {
+        Console.Write("---");
+    }
+
+    Console.WriteLine("+");
+    currentLine--;
+
+
+    for (var y = 0; y < SIZE; y++)
+    {
+        for (var x = 0; x < CurrentShift(currentLine); x++)
+        {
+            Console.Write(" ");
+        }
+
+        Console.Write("|");
+        for (var x = 0; x < SIZE; x++)
+        {
+            Console.Write((x + y) % 2 == 0 ? "   " : "###");
+        }
+
+        Console.Write($"| {SIZE - y}");
+
+
+        Console.WriteLine();
+        currentLine--;
+    }
+
+    for (var x = 0; x < CurrentShift(currentLine); x++)
+    {
+        Console.Write(" ");
+    }
+
+    Console.Write("+");
+    for (var x = 0; x < SIZE; x++)
+    {
+        Console.Write("---");
+    }
+
+    Console.WriteLine("+");
+    currentLine--;
+
+    Console.Write(" ");
+    for (var x = 0; x < SIZE; x++)
+    {
+        Console.Write($" {(char) ('A' + x)} ");
+    }
+
+    Console.WriteLine();
+    currentLine--;
+} while (shift >= 0);
---
+++ 07_Methoden/Übungen/Mirp/Mirp.cs
@@ -0,0 +1,67 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: Mirp
+*--------------------------------------------------------------
+*/
+
+using System;
+
+static bool IsPrim(int number)
+{
+    int i;
+    for (i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+static int Reverse(int number)
+{
+    int reverseNumber = 0;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number = number / 10;
+
+        reverseNumber = reverseNumber * 10 + digit;
+    }
+
+    return reverseNumber;
+}
+
+static bool IstMirp(int number)
+{
+    int reverseNumber = Reverse(number);
+
+    return number != reverseNumber && IsPrim(number) && IsPrim(reverseNumber);
+}
+
+
+Console.WriteLine("Find Mirp Numbers ");
+Console.WriteLine("****************************");
+
+int count = 0;
+
+for (int i = 10; i <= 1000; i++)
+{
+    if (IstMirp(i))
+    {
+        if (count != 0)
+        {
+            Console.Write(",");
+        }
+        if (count != 0 && count % 10 == 0)
+        {
+            Console.WriteLine();
+        }
+        Console.Write(i);
+        count++;
+    }
+}
