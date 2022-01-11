5e4f25dc057d6ff142952185dc6941a739d936d7
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-01-11 15:23:22 +0100

    To Net 6.0

--- 07_Methoden/Demos/IsPrimeUnitTest/MyProject/MyProject.cs
+++ 07_Methoden/Demos/IsPrimeUnitTest/MyProject/MyProject.cs
@@ -7,57 +7,59 @@
 *--------------------------------------------------------------
 */
 
+namespace MyProject;
+
 using System;
 
-namespace MyProject
+public static class Program
 {
-    public static class Program
+    public static bool IsPrime(int number)
     {
-        public static void Main(string[] argv)
+        if (number == 0)
         {
-            Console.WriteLine("Calculate prime numbers");
-            Console.WriteLine("=======================");
-
-            int count = 0;
-
-            for (int i = 10; i <= 1000; i++)
-            {
-                if (IsPrime(i))
-                {
-                    if (count != 0)
-                    {
-                        Console.Write(",");
-                    }
-                    if (count != 0 && count % 10 == 0)
-                    {
-                        Console.WriteLine();
-                    }
-                    Console.Write(i);
-                    count++;
-                }
-            }
+            return false;
         }
 
-        public static bool IsPrime(int number)
+        if (number < 0)
         {
-            if (number == 0)
+            number = -number;
+        }
+
+        for (int i = 2; i <= number / 2; i++)
+        {
+            if (number % i == 0)
             {
                 return false;
             }
+        }
 
-            if (number < 0)
-            {
-                number = -number;
-            }
+        return true;
+    }
 
-            for (int i = 2; i <= number / 2; i++)
+    public static void Main(string[] argv)
+    {
+        Console.WriteLine("Calculate prime numbers");
+        Console.WriteLine("=======================");
+
+        int count = 0;
+
+        for (int i = 10; i <= 1000; i++)
+        {
+            if (IsPrime(i))
             {
-                if (number % i == 0)
+                if (count != 0)
                 {
-                    return false;
+                    Console.Write(",");
                 }
+
+                if (count != 0 && count % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+
+                Console.Write(i);
+                count++;
             }
-            return true;
         }
     }
 }
