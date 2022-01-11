cc91d271ebf4a63c37998d2c74f1968c85f2b69e
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-01-11 14:09:46 +0100

    ROT13

---
+++ 07_Methoden/Übungen/ROT13/ROT13/ROT13.cs
@@ -0,0 +1,58 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: ROT13
+*--------------------------------------------------------------
+*/
+
+using System;
+
+char EncodeChar(char fromCh)
+{
+    char toCh;
+    // nur Kleinbuchstaben verschlüsseln
+    if (fromCh >= 'a' && fromCh <= 'z')
+    {
+        // 13 Positionen vor oder zurück
+        if (fromCh + 13 > 'z')
+        {
+            toCh = (char) (fromCh - 13);
+        }
+        else
+        {
+            toCh = (char) (fromCh + 13);
+        }
+    }
+    else
+    {
+        toCh = fromCh;
+    }
+
+    return toCh;
+}
+
+string Encode(string plainText)
+{
+    var encode = "";
+
+    for (int i = 0; i < plainText.Length; i++)
+    {
+        char fromCh = plainText[i];
+        encode += EncodeChar(fromCh);
+    }
+
+    return encode;
+}
+
+Console.WriteLine("Encryption/Decryption with ROT13");
+Console.Write("Please enter a text: ");
+var plainText = Console.ReadLine();
+
+var encodedText = Encode(plainText);
+var plainAgainText = Encode(encodedText);
+
+Console.WriteLine($"Plain:     {plainText}");
+Console.WriteLine($"Encrypted: {encodedText}");
+Console.WriteLine($"Decrypted: {plainAgainText}");
