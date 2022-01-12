be7f40300e5e6c8b47e02f796347d567ea9dc19c
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-01-12 09:27:44 +0100

    SplitString review (net6.0)

---
+++ 06_Strings/Übungen/StringSplit/StringSplit/StringSplit.cs
@@ -0,0 +1,52 @@
+﻿/***********************************************************************************************
+ * Übungsnr:        14                                     
+ * Programmname:    Split
+ * Autor:           Michael Bucek  
+ * Klasse:          1CHIF
+ * Datum:           06.01.2014                               
+ * ------------------------------------------------ 
+ * Kurzbeschreibung:      
+ * Das Programm ließt zunächst einen beliebigen Text in einen String ein.
+ * Als nächstes wird das gewünschte Trennzeichen eingelesen. Dieses Zeichen soll in einem char
+ * gespeichert werden! (Eingabeprüfung: Länge 1 -> sonst Eingabe wiederholen!)  
+ * Nun soll der eingegebene Text nach dem eingegebenen Trennzeichen aufgesplittet werden.
+ ************************************************************************************************/
+
+using System;
+
+
+Console.Write("Eingabetext:");
+var text = Console.ReadLine();
+
+string input;
+
+do //Trennzeichen einlesen solange bis Länge 1
+{
+    Console.Write("Trennzeichen:");
+    input = Console.ReadLine();
+    if (input.Length != 1)
+        Console.WriteLine("Das Trennzeichen muss genau ein Zeichen sein!");
+} while (input.Length != 1);
+
+var delimiter = Convert.ToChar(input);
+
+var actWord = "";
+
+Console.WriteLine("\nTeiltexte:\n\n");
+for (int actIdx = 0; actIdx < text.Length; actIdx++) //Alle Zeichen des Eingabetextes prüfen
+{
+    if (text[actIdx] == delimiter) //Trennzeichen gefunden
+    {
+        //aktuellen Teiltext ausgeben und Teiltext wieder auf Leerstring setzen
+        Console.WriteLine(actWord);
+        actWord = "";
+    }
+    else
+    {
+        //Kein Trennzeichen -> aktuelles Zeichen zum aktuellen Teiltext hinzufügen
+        actWord = actWord + text[actIdx];
+    }
+}
+
+//Letzten text extra ausgeben (da kein Trennzeichen am Schluss folgt)
+Console.WriteLine(actWord);
