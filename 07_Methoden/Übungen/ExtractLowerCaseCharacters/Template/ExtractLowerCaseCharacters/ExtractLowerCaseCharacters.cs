b7e0c9f6bb3498f175d3350b68e120a71b2d3b06
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-02-07 20:12:42 +0100

    Template for ExtractLowerCaseCharacters

---
+++ 07_Methoden/Übungen/ExtractLowerCaseCharacters/Template/ExtractLowerCaseCharacters/ExtractLowerCaseCharacters.cs
@@ -0,0 +1,39 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: ExtractLowerCaseCharacters with UnitTests
+*--------------------------------------------------------------
+*/
+using System;
+
+namespace ExtractLowerCaseCharacters;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Extract lower case characters");
+        Console.WriteLine("=============================");
+
+        //TODO: Implement main program
+    }
+
+
+    /// <summary>
+    /// Liefert aus dem übergebenen Text ab der Position startPosition (0-basiert) count 
+    /// Kleinbuchstaben zurück. Gibt es nicht mehr so viele Kleinbuchstaben bis zum Textende,
+    /// werden die verfügbaren Kleinbuchstaben zurückgegeben.
+    /// Die ermittelten Kleinbuchstaben werden zu einem Ergebnisstring konkateniert!
+    /// </summary>
+    /// <param name="text">zu analysierender Text</param>
+    /// <param name="startPosition"> ab wo werden die Kleinbuchstaben ermittelt</param>
+    /// <param name="count">Anzahl der gewünschten Kleinbuchstaben</param>
+    /// <returns>Text, der die ermittelten Kleinbuchstaben enthält</returns>
+    public static string ExtractLowerCaseCharacters(string text, int startPosition, int count)
+    {
+        //TODO: Implement method
+        throw new NotImplementedException();
+    }
+}
