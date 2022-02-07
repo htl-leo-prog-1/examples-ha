682f65cc966026402dc84c2f4668a6d20b1e7f6f
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-02-07 20:07:50 +0100

    ExtractLowerCaseCharacters

---
+++ 07_Methoden/Übungen/ExtractLowerCaseCharacters/Solution/ExtractLowerCaseCharacters/ExtractLowerCaseCharacters.cs
@@ -0,0 +1,65 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: ExtractLowerCaseCharacters with UnitTests
+*--------------------------------------------------------------
+*/
+using System;
+
+namespace ExtractLowerCaseCharacters;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Extract lower case characters");
+        Console.WriteLine("=============================");
+
+        Console.Write("Text eingeben: ");
+        string text = Console.ReadLine();
+        
+        Console.Write("Startindex: ");
+        string input = Console.ReadLine();
+        int startIndex = Convert.ToInt32(input);
+        
+        Console.Write("Länge: ");
+        input = Console.ReadLine();
+        
+        var length = Convert.ToInt32(input);
+        var result = ExtractLowerCaseCharacters(text, startIndex, length);
+        Console.WriteLine($"Der Ergebnistext lautet \"{result}\"");
+        Console.WriteLine();
+    }
+
+
+    /// <summary>
+    /// Liefert aus dem übergebenen Text ab der Position startPosition (0-basiert) count 
+    /// Kleinbuchstaben zurück. Gibt es nicht mehr so viele Kleinbuchstaben bis zum Textende,
+    /// werden die verfügbaren Kleinbuchstaben zurückgegeben.
+    /// Die ermittelten Kleinbuchstaben werden zu einem Ergebnisstring konkateniert!
+    /// </summary>
+    /// <param name="text">zu analysierender Text</param>
+    /// <param name="startPosition"> ab wo werden die Kleinbuchstaben ermittelt</param>
+    /// <param name="count">Anzahl der gewünschten Kleinbuchstaben</param>
+    /// <returns>Text, der die ermittelten Kleinbuchstaben enthält</returns>
+    public static string ExtractLowerCaseCharacters(string text, int startPosition, int count)
+    {
+        string result = "";
+        int position = startPosition;
+        while (count > 0 && position < text.Length)
+        {
+            char ch = text[position];
+            if (ch >= 'a' && ch <= 'z')
+            {
+                result += ch;
+                count--;
+            }
+
+            position++;
+        }
+
+        return result;
+    }
+}
---
+++ 07_Methoden/Übungen/ExtractLowerCaseCharacters/Solution/UnitTest/UnitTest.cs
@@ -0,0 +1,78 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: Unittests
+*--------------------------------------------------------------
+*/
+
+namespace UnitTest;
+
+using FluentAssertions;
+using Xunit;
+using ExtractLowerCaseCharacters;
+
+public class ProgramTests
+{
+    [Fact]
+    public void T01_SingleLowerCaseChar()
+    {
+        string text = "x";
+        string expected = "x";
+        int position = 0;
+        int length = 1;
+        Program.ExtractLowerCaseCharacters(text, position, length).Should().Be(expected);
+    }
+
+    [Fact]
+    public void T02_MixedStringSingleLowerCaseChar()
+    {
+        string text = "Hello";
+        string expected = "l";
+        int position = 2;
+        int length = 1;
+        Program.ExtractLowerCaseCharacters(text, position, length).Should().Be(expected);
+    }
+
+    [Fact]
+    public void T03_MixedStringMultipleLowerCaseChars()
+    {
+        string text = "Hello";
+        string expected = "llo";
+        int position = 2;
+        int length = 3;
+        Program.ExtractLowerCaseCharacters(text, position, length).Should().Be(expected);
+        ;
+    }
+
+    [Fact]
+    public void T04_MixedStringTooMuchLowerCaseChars()
+    {
+        string text = "Hello";
+        string expected = "llo";
+        int position = 2;
+        int length = 6;
+        Program.ExtractLowerCaseCharacters(text, position, length).Should().Be(expected);
+    }
+
+    [Fact]
+    public void T05_MixedStringTooMuchMixedCaseChars()
+    {
+        string text = "HeLLo";
+        string expected = "eo";
+        int position = 1;
+        int length = 3;
+        Program.ExtractLowerCaseCharacters(text, position, length).Should().Be(expected);
+    }
+
+    [Fact]
+    public void T06_OnlyUpperCaseText()
+    {
+        string text = "HELLO";
+        string expected = "";
+        int position = 1;
+        int length = 3;
+        Program.ExtractLowerCaseCharacters(text, position, length).Should().Be(expected);
+    }
+}
