bdfe20552c2c2ca4b40bc718d2405f1d746de2c1
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-02-15 11:39:01 +0100

    Letter Count

---
+++ 08_Arrays/Übungen/LetterCounter/Solution/LetterCounter.cs
@@ -0,0 +1,61 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: AverageGrade
+*--------------------------------------------------------------
+*/
+
+namespace LetterCounter
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Letter-Count");
+
+            Console.Write("Please enter a text: ");
+            var input = Console.ReadLine();
+            int noLetterCounts;
+
+            var letterCounts = CountLetters(input, out noLetterCounts);
+
+            PrintCounts(letterCounts,noLetterCounts);
+        }
+
+        public static int[] CountLetters(string str, out int noLetterCounts)
+        {
+            var letterCounts = new int[26];
+            noLetterCounts = 0;
+
+            foreach (char ch in str)
+            {
+                int letterIndex = Char.ToLower(ch) - 'a';
+                if (letterIndex >= 0 && letterIndex < letterCounts.Length)
+                {
+                    letterCounts[letterIndex]++;
+                }
+                else
+                {
+                    noLetterCounts++;
+                }
+            }
+
+            return letterCounts;
+        }
+
+        public static void PrintCounts(int[] letterCounts, int noLetterCounts)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < letterCounts.Length / 2; i++)
+            {
+                Console.Write($"{(char)(i + 'A')}: {letterCounts[i]}\t");
+                Console.WriteLine($"{(char)(i + 13 + 'A')}: {letterCounts[i + 13]}\t");
+            }
+            Console.WriteLine($"Other: {noLetterCounts}");
+        }
+    }
+}
