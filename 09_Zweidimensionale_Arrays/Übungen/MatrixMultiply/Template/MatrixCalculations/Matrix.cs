87d65d45347cd9b4888677402fa6a320645af4cc
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-03-01 14:49:12 +0100

    Matrix Calculation

---
+++ 09_Zweidimensionale_Arrays/Übungen/MatrixMultiply/Template/MatrixCalculations/Matrix.cs
@@ -0,0 +1,36 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: MatrixCalculation
+*--------------------------------------------------------------
+*/
+
+namespace MatrixCalculations
+{
+    using System;
+
+    public class Matrix
+    {
+        public static void PrintMatrix(int[,] matrix)
+        {
+            // TODO: Implement here
+        }
+
+        public static int[,] MultiplyMatrix(int[,] matrixA, int[,] matrixB)
+        {
+            // TODO: Implement here
+        }
+
+        public static int MultiplyPosition(int[,] matrix1, int[,] matrix2, int size, int row, int col)
+        {
+            // TODO: Implement here
+        }
+
+        public static bool CompareMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            // TODO: Implement here
+        }
+    }
+}
---
+++ 09_Zweidimensionale_Arrays/Übungen/MatrixMultiply/Template/UnitTest/MatrixTests.cs
@@ -0,0 +1,93 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: Matrixtest UnitTests
+*--------------------------------------------------------------
+*/
+
+namespace UnitTest
+{
+    using FluentAssertions;
+    using MatrixCalculations;
+    using Xunit;
+
+    public class MatrixTests
+    {
+        private void TestMatrixMultiply(int[,] matrixA, int[,] matrixB, int[,] expectedResult)
+        {
+            Matrix.CompareMatrix(matrixA, matrixA).Should().BeTrue("CompareMatrix: Vergleich identer Matrizen");
+            Matrix.CompareMatrix(matrixA, matrixB).Should().BeFalse("CompareMatrix: Vergleich verschiedener Matrizen");
+            var matrixMultiplied = Matrix.MultiplyMatrix(matrixA, matrixB);
+            Matrix.CompareMatrix(expectedResult, matrixMultiplied).Should().BeTrue("Matrixmultipikation richtig");
+        }
+        [Fact]
+        public void T01_Multiply()
+        {
+            int[,] matrix1a =
+            {
+                {3, 2, 1},
+                {4, 7, 9}
+            };
+            int[,] matrix1b =
+            {
+                {2, 8},
+                {3, 5},
+                {6, 1}
+            };
+            int[,] expectedResult1 =
+            {
+                {18, 35},
+                {83, 76}
+            };
+
+            TestMatrixMultiply(matrix1a, matrix1b, expectedResult1);
+
+        }
+
+        [Fact]
+        public void T02_Multiply()
+        {
+            int[,] matrix2a =
+            {
+                {3, 6, 7},
+                {5, 3, 5},
+                {6, 2, 9}
+            };
+            int[,] matrix2b =
+            {
+                {1, 2, 7},
+                {0, 9, 3},
+                {6, 0, 6}
+            };
+            int[,] expectedResult2 =
+            {
+                {45, 60, 81},
+                {35, 37, 74},
+                {60, 30, 102}
+            };
+
+            TestMatrixMultiply(matrix2a, matrix2b, expectedResult2);
+        }
+
+        [Fact]
+        public void T03_Multiply()
+        {
+            int[,] matrix3a =
+            {
+                {4}
+            };
+            int[,] matrix3b =
+            {
+                {7}
+            };
+            int[,] expectedResult3 =
+            {
+                {28}
+            };
+
+            TestMatrixMultiply(matrix3a, matrix3b, expectedResult3);
+        }
+    }
+}
