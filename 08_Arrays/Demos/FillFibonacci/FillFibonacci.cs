6dd5584d92f3b1c64de14606a066ac8ec62f9b30
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-03-01 13:38:47 +0100

    MatrixMultiply

---
+++ 08_Arrays/Demos/FillFibonacci/FillFibonacci.cs
@@ -0,0 +1,51 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: FibonacciArray
+*--------------------------------------------------------------
+*/
+
+namespace FibonacciArray
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci-Array");
+            Console.WriteLine("**********************");
+
+            Console.Write("Please max number (index): ");
+            int maxIndex = int.Parse(Console.ReadLine());
+
+            var numbers = CreateFibonacciArray(maxIndex);
+            WriteArray(numbers);
+        }
+
+        public static int[] CreateFibonacciArray(int maxIndex)
+        {
+            int[] fibonacci = new int[maxIndex];
+
+            fibonacci[0] = 1;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < fibonacci.Length; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+
+            return fibonacci;
+        }
+
+        public static void WriteArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. Fibonacci: {numbers[i]}");
+            }
+        }
+    }
+}
---
+++ 08_Arrays/Demos/ReadModifyWriteArray/ReadModifyWriteArray.cs
@@ -0,0 +1,71 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: ReadModifyWriteAray
+*--------------------------------------------------------------
+*/
+
+namespace ReadModifyWriteAray
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ReadModifyWriteArray");
+            Console.WriteLine("**********************");
+
+            var numbers = ReadArray();
+            var numbersCopy = CopyArray(numbers);
+            ModifyArray(numbers);
+            WriteArray(numbersCopy);
+            WriteArray(numbers);
+        }
+
+        public static int[] ReadArray()
+        {
+            Console.Write("Please enter the count of numbers: ");
+            int countOfNumbers = int.Parse(Console.ReadLine());
+            
+            int[] intNumbers = new int[countOfNumbers];
+
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                Console.Write($"Please enter {i + 1}. number: ");
+                intNumbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            return intNumbers;
+        }
+
+        public static void ModifyArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i] * 2; 
+            }
+        }
+
+        public static void WriteArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. number: {numbers[i]}");
+            }
+        }
+
+        public static int[] CopyArray(int[] numbers)
+        {
+			var newNumbers = new int[numbers.Length];
+			
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newNumbers[i] = numbers[i]; 
+            }
+			return newNumbers;
+        }
+    }
+}
