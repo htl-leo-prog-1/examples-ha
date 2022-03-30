33d6169590e0cf3ae7ef6f75f534798ece15dd7a
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-03-30 16:13:07 +0200

    FussballMeisterschaft

--- 11_Datenkapsel/Übungen/FussballMeisterschaft/Solution/FussballMeisterschaft/Program.cs
+++ 11_Datenkapsel/Übungen/FussballMeisterschaft/Solution/FussballMeisterschaft/Program.cs
@@ -23,21 +23,21 @@ public class Program
     {
         MenuExit,
         MenuSelectCurrentTeam,
-        MenuFilterTeam
+        MenuPrintAllGames,
+        MenuPrintCurrentTeamGames,
+        MenuPrintCurrentTeamGoals,
+        MenuPrintAllTeams
     }
 
     static void Main(string[] args)
     {
-        Console.WriteLine("EM-Qualifikation 2020");
+        Console.WriteLine("Fussball-Meisterschaft");
         Console.WriteLine("=====================");
 
         var allGames = ReadGamesFromFile(fileName);
-
-        PrintGames(allGames);
-        Console.WriteLine();
-
         var currentTeam = string.Empty;
-        var menuSelection = ShowMenu(currentTeam);
+
+        var menuSelection = ShowMenu(currentTeam, allGames);
 
         while (menuSelection != MenuSelection.MenuExit)
         {
@@ -46,26 +46,52 @@ public class Program
                 case MenuSelection.MenuSelectCurrentTeam:
                     currentTeam = ReadTeamName();
                     break;
-                case MenuSelection.MenuFilterTeam:
+                case MenuSelection.MenuPrintAllGames:
+                    PrintGames(allGames);
+                    break;
+                case MenuSelection.MenuPrintCurrentTeamGames:
                     var filteredGames = FilterGamesByCountryName(allGames, currentTeam);
                     PrintGames(filteredGames);
-                    PrintGoals(filteredGames, currentTeam);
+                    break;
+                case MenuSelection.MenuPrintCurrentTeamGoals:
+                    PrintGoals(allGames, currentTeam);
+                    break;
+                case MenuSelection.MenuPrintAllTeams:
+                    PrintTeams(allGames);
                     break;
             }
-            menuSelection = ShowMenu(currentTeam);
+
+            menuSelection = ShowMenu(currentTeam, allGames);
         }
     }
 
-    private static MenuSelection ShowMenu(string currentTeam)
+    private static MenuSelection ShowMenu(string currentTeam, Game[] allGames)
     {
         MenuSelection? menuSelection = null;
 
-        Console.WriteLine("Menu");
+        Console.WriteLine($"Menu (Games:{allGames.Length})");
 
         do
         {
-            Console.WriteLine("1: Select Team");
+            if (string.IsNullOrEmpty(currentTeam))
+            {
+                Console.WriteLine("1: Select team ");
+            }
+            else
+            {
+                Console.WriteLine($"1: Select other team - current : {currentTeam}");
+            }
+
+            Console.WriteLine("2: print all games");
+            if (!string.IsNullOrEmpty(currentTeam))
+            {
+                Console.WriteLine("3: print games of current team");
+                Console.WriteLine("4: goals of current team");
+            }
+
+            Console.WriteLine("5: print all teams");
             Console.WriteLine("X: Exit");
+            Console.Write("=> ");
             string userInput = Console.ReadLine();
 
             switch (userInput)
@@ -73,6 +99,27 @@ public class Program
                 case "1":
                     menuSelection = MenuSelection.MenuSelectCurrentTeam;
                     break;
+                case "2":
+                    menuSelection = MenuSelection.MenuPrintAllGames;
+                    break;
+                case "3":
+                    if (!string.IsNullOrEmpty(currentTeam))
+                    {
+                        menuSelection = MenuSelection.MenuPrintCurrentTeamGames;
+                    }
+
+                    break;
+                case "4":
+                    if (!string.IsNullOrEmpty(currentTeam))
+                    {
+                        menuSelection = MenuSelection.MenuPrintCurrentTeamGoals;
+                    }
+
+                    break;
+                case "5":
+                    menuSelection = MenuSelection.MenuPrintAllTeams;
+                    break;
+                case "x":
                 case "X":
                     menuSelection = MenuSelection.MenuExit;
                     break;
@@ -128,7 +175,7 @@ public class Program
 
     private static void PrintGames(Game[] games)
     {
-        var header = $"{"Datum",-10} {"Home-Team",-15} {"Guest-Team",-15}";
+        var header = $"{"Date",-10} {"Home-Team",-15} {"Guest-Team",-15}";
 
         Console.WriteLine(header);
         Console.WriteLine(new String('=', header.Length + 4));
@@ -147,7 +194,23 @@ public class Program
 
         CountGoals(games, countryName, out goals, out gotGoals);
 
-        Console.WriteLine($"Tore {goals}:{gotGoals} Tordifferenz: {goals - gotGoals}");
+        Console.WriteLine($"Goals {goals}:{gotGoals} Difference: {goals - gotGoals}");
+    }
+
+    private static void PrintTeams(Game[] games)
+    {
+        var teams = Sort(CreateListOfTeams(games));
+
+        var header = $"{"Team",-15} {"SP",3} {"S",3} {"N",3} {"U",3} {"Tore",6} {"+/-",6} {"Pt",4}";
+
+        Console.WriteLine(header);
+        Console.WriteLine(new String('=', header.Length + 4));
+
+        foreach (var team in teams)
+        {
+            Console.WriteLine(
+                $"{team.TeamName,-15} {team.Games,3} {team.Win,3} {team.Loss,3} {team.Tie,3} {team.GoalVsGot,6} {team.GoalDiff,6} {team.Points,4}");
+        }
     }
 
     public static void CountGoals(Game[] games, string countryName, out int goals, out int gotGoals)
@@ -187,6 +250,42 @@ public class Program
         return Copy(filteredGames, count);
     }
 
+    private static Team[] CreateListOfTeams(Game[] games)
+    {
+        var teams = new Team[games.Length];
+        int teamCount = 0;
+        
+        foreach (var game in games)
+        {
+            int teamIdx = IndexOf(teams, teamCount, game.HomeTeam);
+            if (teamIdx < 0)
+            {
+                teams[teamCount] = new Team();
+                teams[teamCount].TeamName = game.HomeTeam;
+                teamIdx = teamCount;
+                teamCount++;
+            }
+
+            teams[teamIdx].Goals += game.GoalsHome;
+            teams[teamIdx].Got += game.GoalsGuest;
+            teams[teamIdx].Win += game.GoalsHome > game.GoalsGuest ? 1 : 0;
+            teams[teamIdx].Loss += game.GoalsHome < game.GoalsGuest ? 1 : 0;
+            teams[teamIdx].Tie += game.GoalsHome == game.GoalsGuest ? 1 : 0;
+        }
+
+        foreach (var game in games)
+        {
+            int teamIdx = IndexOf(teams, teamCount, game.GuestTeam);
+            teams[teamIdx].Got += game.GoalsHome;
+            teams[teamIdx].Goals += game.GoalsGuest;
+            teams[teamIdx].Win += game.GoalsHome < game.GoalsGuest ? 1 : 0;
+            teams[teamIdx].Loss += game.GoalsHome > game.GoalsGuest ? 1 : 0;
+            teams[teamIdx].Tie += game.GoalsHome == game.GoalsGuest ? 1 : 0;
+        }
+
+        return Copy(teams, teamCount);
+    }
+
     private static bool IsCountryGame(Game game, string countryName)
     {
         return IsCountryGame(game.HomeTeam, countryName)
@@ -198,9 +297,56 @@ public class Program
         return countryName.ToUpper().Contains(lookForCountryName.ToUpper());
     }
 
+    private static Team[] Sort(Team[] teams)
+    {
+        teams = Copy(teams, teams.Length);
+
+        for (int i = 0; i < teams.Length; i++)
+        {
+            int min = i;
+            for (int j = i + 1; j < teams.Length; j++)
+            {
+                if (teams[j].Points > teams[min].Points ||
+                    (teams[j].Points == teams[min].Points && teams[j].GoalDiff > teams[min].GoalDiff))
+                {
+                    min = j;
+                }
+            }
+
+            var tmp = teams[i];
+            teams[i] = teams[min];
+            teams[min] = tmp;
+        }
+
+        return teams;
+    }
+
+    private static int IndexOf(Team[] teams, int count, string teamName)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (teams[i].TeamName == teamName)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     private static Game[] Copy(Game[] src, int count)
     {
         var dest = new Game[count];
+        for (int i = 0; i < count; i++)
+        {
+            dest[i] = src[i];
+        }
+
+        return dest;
+    }
+    private static Team[] Copy(Team[] src, int count)
+    {
+        var dest = new Team[count];
         for (int i = 0; i < count; i++)
         {
             dest[i] = src[i];
---
+++ 11_Datenkapsel/Übungen/FussballMeisterschaft/Solution/FussballMeisterschaft/Team.cs
@@ -0,0 +1,27 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: Fussballmeisterschaft
+*--------------------------------------------------------------
+*/
+
+namespace FussballMeisterschaft;
+
+using System;
+
+public class Team
+{
+    public string TeamName { get; set; }
+    public int Win { get; set; }
+    public int Loss { get; set; }
+    public int Tie { get; set; }
+    public int Goals { get; set; }
+    public int Got { get; set; }
+
+    public int Games => Win+Loss+Tie;
+    public int Points => Win*3 + Tie;
+    public int GoalDiff => Goals - Got;
+    public string GoalVsGot => $"{Goals}:{Got}";
+}
