37dad51e36df083ed8e251b7ee4a731f787a0bea
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-03-30 08:59:17 +0200

    EMQualifikation für Abendschule

---
+++ 11_Datenkapsel/Übungen/EMQualifikation/2022/Template/EMQualification/Game.cs
@@ -0,0 +1,13 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: EMQualification
+*--------------------------------------------------------------
+*/
+
+namespace EMQualification
+{
+    //TODO Implement "Game" class here, properties with get and set: "Date", "HomeTeam", "GuestTeam", "GoalsHome", "GoalsTest"
+}
