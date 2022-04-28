e6bc949b2f485399208caac2e7fb95dacdbfb1cd
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2022-04-28 18:52:05 +0200

    MineSweeper (new version), work

---
+++ 10_Games/Übungen/MineSweeper/Solution/MineSweeper/MineSweeper.cs
@@ -0,0 +1,309 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: MineSweeper
+*--------------------------------------------------------------
+*/
+
+namespace MineSweeper;
+
+using System;
+
+public class MineSweeper
+{
+    public static void Run()
+    {
+        Console.WriteLine("MineSweeper");
+        Console.WriteLine("===========");
+
+        var size  = Tools.ReadNumber("Size of (squared) board ", 20,          2);
+        var mines = Tools.ReadNumber("Count of mines ",          size * size, 1);
+
+        InitBoard(size, size);
+
+        var mineField = CreateMineField(mines, size, size);
+
+        PlayGame(mineField);
+        ShowMines(mineField);
+
+        Console.ReadLine();
+        Board.Exit();
+    }
+
+    /// <summary>
+    /// Initialize the board.
+    /// Set all fields to \u2593
+    /// </summary>
+    /// <param name="rows">Rows of the mine-field</param>
+    /// <param name="cols">Cols of the mine-field</param>
+    private static void InitBoard(int rows, int cols)
+    {
+        Board.Init(rows, cols, "MineSweeper");
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                Board.SetText(row, col, "\u2593", "LightGray");
+            }
+        }
+    }
+
+    private static void PlayGame(bool[,] mineField)
+    {
+        int  rows      = mineField.GetLength(0);
+        int  cols      = mineField.GetLength(1);
+
+        bool mineOrEnd = false;
+
+        do
+        {
+            int  row;
+            int  col;
+            bool markAsMine;
+
+            ReadRowCol(rows, cols, out row, out col, out markAsMine);
+
+            if (markAsMine)
+            {
+                MarkAsMine(mineField, row, col);
+            }
+            else
+            {
+                mineOrEnd = !ClearField(mineField, row, col);
+            }
+        } while (!mineOrEnd);
+    }
+
+    /// <summary>
+    /// Ask the user for the next move.  
+    /// </summary>
+    /// <param name="rows">Max count of rows.</param>
+    /// <param name="cols">Max count of columns.</param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <param name="markAsMine"></param>
+    /// <returns>true if ok, false to end the program</returns>
+    static bool ReadRowCol(int rows, int cols, out int row, out int col, out bool markAsMine)
+    {
+        bool isOk;
+        do
+        {
+            row        = 0;
+            col        = 0;
+            markAsMine = false;
+
+            Console.Write("Clear field (e.g.: 4,4) or set as mine (e.g.: *3,4): ");
+            string input = Console.ReadLine() ?? "".Trim();
+
+            if (input.Length > 0 && input[0] == '*')
+            {
+                markAsMine = true;
+                input      = input.Substring(1);
+            }
+
+            var rowCol = input.Split(',');
+
+            isOk = rowCol.Length == 2 &&
+                   int.TryParse(rowCol[0], out row) && row >= 0 && row < rows &&
+                   int.TryParse(rowCol[1], out col) && col >= 0 && col < cols;
+        } while (!isOk);
+
+        return true;
+    }
+
+    /// <summary>
+    /// Mark a field as a mine.
+    /// </summary>
+    /// <param name="mineField">Our mine-field</param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    static void MarkAsMine(bool[,] mineField, int row, int col)
+    {
+        Board.SetText(row, col, "\u25B6", "Red");
+    }
+
+    /// <summary>
+    /// Clear a field on the mine-field.
+    /// If a mine is hit, false is returned.
+    /// </summary>
+    /// <param name="mineField">Our mine-field</param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <returns>true if on mine is hit, otherwise false</returns>
+    static bool ClearField(bool[,] mineField, int row, int col)
+    {
+        if (mineField[row, col])
+        {
+            // hit a mine!
+            return false;
+        }
+
+        ClearAllFields(mineField, row, col);
+
+        return true;
+    }
+
+    /// <summary>
+    /// Clear field and all surrounding (if mine-count == 0)
+    /// </summary>
+    /// <param name="mineField">Our mine-field</param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+
+    static void ClearAllFields(bool[,] mineField, int row, int col)
+    {
+        int rows = mineField.GetLength(0);
+        int cols = mineField.GetLength(1);
+
+        if (IsInRange(row, rows) && IsInRange(col, cols))
+        {
+            if (Board.GetText(row, col).Length > 0)
+            {
+                int mines = CountMinesAround(mineField, row, col);
+                if (mines == 0)
+                {
+                    Board.SetText(row, col, "");
+                    ClearAllFields(mineField, row + 1, col);
+                    ClearAllFields(mineField, row - 1, col);
+                    ClearAllFields(mineField, row,     col + 1);
+                    ClearAllFields(mineField, row,     col - 1);
+                }
+                else
+                {
+                    var colors = new[] { "", "Blue", "Green", "Red", "DarkBlue", "DarkRed", "BlueViolet", "Black", "Black" };
+                    Board.SetText(row, col, mines.ToString(), colors[mines]);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// After an error (the user has hit a mine),
+    /// we show the hidden mines.
+    /// </summary>
+    /// <param name="mineField"></param>
+    static void ShowMines(bool[,] mineField)
+    {
+        int rows = mineField.GetLength(0);
+        int cols = mineField.GetLength(1);
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                if (mineField[row, col])
+                {
+                    Board.SetText(row, col, "\u2B24", "Red");
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Count the mines around a given position.
+    /// </summary>
+    /// <param name="mineField">Our mine-field</param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <returns>Count of mine excluded th current position.</returns>
+    public static int CountMinesAround(bool[,] mineField, int row, int col)
+    {
+        int rows = mineField.GetLength(0);
+        int cols = mineField.GetLength(1);
+
+        int count   = 0;
+        int rowFrom = Math.Max(0, row - 1);
+        int rowTo   = Math.Min(rows - 1, row + 1);
+        int colFrom = Math.Max(0, col - 1);
+        int colTo   = Math.Min(cols - 1, col + 1);
+
+        for (int myRow = rowFrom; myRow <= rowTo; myRow++)
+        {
+            for (int myCol = colFrom; myCol <= colTo; myCol++)
+            {
+                if (mineField[myRow, myCol])
+                {
+                    count++;
+                }
+            }
+        }
+
+        if (mineField[row, col])
+        {
+            count--;
+        }
+
+        return count;
+    }
+
+    /// <summary>
+    /// Count all mines in the mine-field
+    /// </summary>
+    /// <param name="mineField"></param>
+    /// <returns>Count of all mines in the mine-field.</returns>
+    public static int CountMinesOnBoard(bool[,] mineField)
+    {
+        int count = 0;
+        for (int row = 0; row < mineField.GetLength(0); row++)
+        {
+            for (int col = 0; col < mineField.GetLength(1); col++)
+            {
+                if (mineField[row, col])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /// <summary>
+    /// Create a mine-field with mines on random positions.
+    /// </summary>
+    /// <param name="countMines">Count of mines to be place on the mine-field</param>
+    /// <param name="rows">Mine-Field rows</param>
+    /// <param name="cols">Mine-Field columns</param>
+    /// <returns>Created mine-field, if not valid (e.g. to many mines) null</returns>
+    public static bool[,] CreateMineField(int countMines, int rows, int cols)
+    {
+        var random = new Random();
+
+        if (countMines > (rows * cols))
+        {
+            return null;
+        }
+
+        var mineField = new bool[rows, cols];
+
+        for (int i = 0; i < countMines; i++)
+        {
+            int row;
+            int col;
+
+            do
+            {
+                row = random.Next(rows);
+                col = random.Next(cols);
+            } while (mineField[row, col]);
+
+            mineField[row, col] = true;
+        }
+
+        return mineField;
+    }
+
+    /// <summary>
+    /// Helper to look, if a row/col is in range
+    /// </summary>
+    /// <param name="rowOrCol"></param>
+    /// <param name="size"></param>
+    /// <returns>true if ok, false otherwise</returns>
+    static bool IsInRange(int rowOrCol, int size)
+    {
+        return rowOrCol >= 0 && rowOrCol < size;
+    }
+}
---
+++ 10_Games/Übungen/MineSweeper/Solution/MineSweeper/Tools.cs
@@ -0,0 +1,28 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: MineSweeper
+*--------------------------------------------------------------
+*/
+
+using System;
+
+namespace MineSweeper;
+
+public static class Tools
+{
+    public static int ReadNumber(string message, int max, int min)
+    {
+        int number;
+        bool isOk;
+        do
+        {
+            Console.Write($"{message} [{min}..{max}]: ");
+            isOk = int.TryParse(Console.ReadLine(), out number) && number >= min && number <= max;
+        } while (!isOk);
+
+        return number;
+    }
+}
