3c669a24dfe0a8a1f9eac1fda4adbb12d0fcafba
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2023-06-21 19:07:25 +0200

    add ConvertV2

---
+++ 12_Datenkapsel/Übungen/FussballMeisterschaft/ConvertToCsvV2/Game.cs
@@ -0,0 +1,20 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: ZweiteLiga
+*--------------------------------------------------------------
+*/
+
+using System;
+using System.IO;
+
+class Game
+{
+    public DateTime Date { get; set; }
+    public string HomeTeam { get; set; }
+    public string GuestTeam { get; set; }
+    public string Result { get; set; }
+    public string Result2 { get; set; }
+}
