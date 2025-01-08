ee565167c40f2654225a2ef95c6485423d104c65
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2025-01-08 11:36:55 +0100

    HexDump

---
+++ 06_Strings/Übungen/HexDump/HexDump.cs
@@ -0,0 +1,65 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: HexDump
+ * Gibt einen String in Hex Darstellug aus
+ *--------------------------------------------------------------
+ */
+
+using System;
+
+Console.WriteLine("HexDump");
+Console.WriteLine("****************************");
+
+const int CountPerLine = 8;
+
+string toHex = "0123456789ABCDEF";
+string text = "";
+
+Console.Write("Please enter a string (or END): ");
+string input = Console.ReadLine();
+
+while (input != null && input != "END")
+{
+    if (text != "")
+    {
+        text += "\n";
+    }
+
+    text += input;
+    Console.Write("Please enter a string (or END): ");
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("****************************");
+
+string asAscii = "";
+
+for (int i = 0; i < text.Length; i++)
+{
+    char ch = text[i];
+
+    asAscii += char.IsLetterOrDigit(ch) ? ch : '.';
+
+    Console.Write($"{toHex[ch / 16]}{toHex[ch % 16]} ");
+
+    if (i % CountPerLine == (CountPerLine - 1))
+    {
+        Console.WriteLine($"'{asAscii}'");
+        asAscii = "";
+    }
+}
+
+if (!string.IsNullOrEmpty(asAscii))
+{
+    for (int i = 0; i < CountPerLine - (text.Length % CountPerLine); i++)
+    {
+        Console.Write("   ");
+    }
+
+    Console.WriteLine($"'{asAscii}'");
+}
+
+Console.WriteLine("****************************");
