b5b7f178d94865d60e7c1719f63fd99e06944b41
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2025-02-05 10:16:01 +0100

    IsEvenOdd with Unittests

---
+++ 07_Methoden/Übungen/IsEvenOddNumber/SolutionUnitTest/IsEvenOddNumber/Program.cs
@@ -0,0 +1,104 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: IsEvenOddNumber with UnitTests
+ *--------------------------------------------------------------
+ */
+
+using System;
+
+namespace IsEvenOddNumber
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Find Even/Odd Number Numbers ");
+            Console.WriteLine("****************************");
+
+            int calculateFrom = ReadNumber("Calculate from", int.MaxValue, int.MinValue);
+            int calculateTo   = ReadNumber("Calculate to",   int.MaxValue, calculateFrom);
+
+            Console.WriteLine("****************************");
+
+            int count = 0;
+
+            for (int i = calculateFrom; i <= calculateTo; i++)
+            {
+                if (IsEvenOddNumber(i))
+                {
+                    if (count != 0)
+                    {
+                        Console.Write(",");
+                    }
+
+                    if (count != 0 && count % 10 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+
+                    Console.Write(i);
+
+                    count++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("****************************");
+            Console.WriteLine($"They are {count} numbers in the range of [{calculateFrom}..{calculateTo}]");
+            Console.WriteLine("****************************");
+        }
+
+        public static bool IsEvenOddNumber(int number)
+        {
+            int sumOfDigitEven = SumOfDigitEven(number);
+            int sumOfDigitOdd  = SumOfDigitOdd(number);
+
+            return number > 0 && sumOfDigitEven == sumOfDigitOdd;
+        }
+
+        private static int SumOfDigitEven(int number)
+        {
+            return SumOfDigit(number, 0);
+        }
+
+        private static int SumOfDigitOdd(int number)
+        {
+            return SumOfDigit(number, 1);
+        }
+
+        private static int SumOfDigit(int number, int remainder)
+        {
+            int sumOfDigit = 0;
+            number = Math.Abs(number);
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == remainder)
+                {
+                    sumOfDigit += digit;
+                }
+
+                number /= 10;
+            }
+
+            return sumOfDigit;
+        }
+
+        private static int ReadNumber(string message, int max, int min)
+        {
+            int  number;
+            bool isOk;
+            do
+            {
+                Console.Write($"{message} [{min}..{max}]: ");
+                isOk = int.TryParse(Console.ReadLine(), out number) && number >= min && number <= max;
+            } while (!isOk);
+
+            return number;
+        }
+    }
+}
---
+++ 07_Methoden/Übungen/IsEvenOddNumber/TemplateUnitTest/UnitTest/ProgramTests.cs
@@ -0,0 +1,62 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: IsEvenOddNumber UnitTests
+ *--------------------------------------------------------------
+ */
+
+using FluentAssertions;
+
+using Xunit;
+
+using System.Linq;
+
+using IsEvenOddNumber;
+
+namespace UnitTest
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void T01_TestOk9887()
+        {
+            Program.IsEvenOddNumber(9887).Should().BeTrue();
+        }
+
+        [Fact]
+        public void T02_TestErrorMinus9887()
+        {
+            Program.IsEvenOddNumber(-9887).Should().BeFalse();
+        }
+
+        readonly int[] _evenOdds0To1000 = new[]
+        {
+            112, 121, 134, 143, 156, 165, 178, 187, 211, 314,
+            336, 341, 358, 363, 385, 413, 431, 516, 538, 561,
+            583, 615, 633, 651, 718, 781, 817, 835, 853, 871
+        };
+
+        [Fact]
+        public void T03_TestOk0To1000()
+        {
+            foreach (var number in _evenOdds0To1000)
+            {
+                Program.IsEvenOddNumber(number).Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public void T04_TestError0To1000()
+        {
+            for (int i = -1000; i <= 1000; i++)
+            {
+                if (!_evenOdds0To1000.Contains(i))
+                {
+                    Program.IsEvenOddNumber(i).Should().BeFalse();
+                }
+            }
+        }
+    }
+}
