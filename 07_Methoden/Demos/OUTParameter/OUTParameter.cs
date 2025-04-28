7f5ef5fbe6e0263c4133a3ece342e970fd12b0b5
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2025-04-28 09:48:12 +0200

    MinMax out parameter

---
+++ 07_Methoden/Demos/OUTParameter/OUTParameter.cs
@@ -0,0 +1,44 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: Eingabe Verarbeitung Ausgabe, Parameterübergabe
+*--------------------------------------------------------------
+*/
+
+using System;
+
+void GetMinAndMax(int[] array, out int min, out int max)
+{
+    min = int.MaxValue;
+    max = int.MinValue;
+
+    foreach (int val in array)
+    {
+        if (val > max)
+        {
+            max = val;
+        }
+        else if (val < min)
+        {
+            min = val;
+        }
+    }
+}
+
+
+int[] myArray = new int[100];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = Random.Shared.Next();
+}
+
+
+int myMin;
+int myMax;
+
+GetMinAndMax(myArray, out myMin, out myMax);
+
+Console.WriteLine($"min={myMin}, max={myMax}");
