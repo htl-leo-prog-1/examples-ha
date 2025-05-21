ac55c981904c61937e6c5f81b9d406fee854c7b4
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2025-05-21 17:23:43 +0200

    BattleShipV2

---
+++ 10_Games/Übungen/BattleShipV2/BattleShip.md
@@ -0,0 +1,80 @@
+c# Programmieren (c) HTL-Leonding
+
+# BattleShip - Schiffe versenken
+
+## Lehrziele
+
+* mehrdimensionale Arrays
+* Datei Lesen/Schreiben
+* Board
+
+## Aufgabenstellung
+
+Gesucht ist ein Programm für das Spiel **Schiffe versenken**.  
+In dieser Varianten soll "nur" das Erstellen eines Spielfelds unterstützt werden.  
+Der Anwender kann durch eine Konsolen-Eingabe Schiffe am Spielfeld setzen (oder auch wieder löschen). Das Spielfeld soll in einer Datei speichert und aus einer Datei wieder geladen werden können.  
+
+### Programmablauf
+Schreiben Sie ein Programm **BattleShip** mit folgenden Eigenschaften:  
+
+* Beim Programmstart wird die Dimension des Spielfelds eingegeben.  
+  Da es sich um ein quadratisches Spielfeld handelt, muss nur eine Dimension eingegeben werden, z.B.: `15`.  
+  Erlaubte Größen sind 10x10, 15x15 oder 20x20.
+* Anschließend wird das **Board** mit den eingegebenen Dimensionen erstellt.
+* Der Benutzer wird aufgefordert, die folgenden Kommandos einzugeben:
+  * **exit**  
+    Das Programm wird sofort (ohne Rückfrage) beendet.  
+  * **load** *filename*  
+    Mit dieser Eingabe wird das aktuelle Spielfelde zurückgesetzt und die Schiffe aus der angegebenen Datei geladen.  
+    Nur gültige Spielfelder werden geladen. Sind die Positionen der Schiffe ungültig, wird eine Meldung ausgegeben und das Programm verlassen.  
+    Beispiel: *load mygame.csv*  
+  * **save** *filename*  
+    Das aktuelle Spielfeld wird in die angegebene Datei gespeichert.  
+    Eine eventuell vorhandene Datei wird NICHT überschrieben sondern auf *bak* umbenannt.  
+  * **row,col**  
+    Beispiel: mit *3,5* wird an die Stelle 3,5 (row,col) eine neues Schiff positioniert.  
+    Im Board wird die Position mit 'o' belegt.  
+    Eine Fehlermeldung ist auszugeben, wenn das Schiff nicht gesetzt werden kann:
+    * Wenn im Feld bereits ein Schiff an dieser Position vorhanden ist.  
+    * In den Nachbarfeldern (horizontal, vertikal und diagonal) ein Schiff existiert.  
+      Ein Schiff darf an eine Randposition gesetzt werden.  Die Nachbarpositionen, die ausserhalb des Feldes liegen, werden ignoriert.  
+* **!row,col**  
+   Mit dieser Eingabe wird ein Schiff gelöscht. Eine Fehlermeldung ist auszugeben, wenn and der Position kein Schiff gespeichert ist.  
+
+### Programmdesign
+
+Achten Sie bei der Umsetzung auf ein sauberes Design Ihres Programms.  
+In dem bereitgestellten Programm-Template sind Unittests vorhanden. Implementieren Sie die Methoden so, dass **alle** Unittests erfolgreich ausgeführt werden können. Änderungen an den *Unittests* sind nicht erlaubt.  
+Damit die Unittests ausgeführt werden können, müssen sie folgende (Hilfs-)Methoden umsetzen:
+
+* `bool CanSetShip(int[,] battlefield, int row, int col)`  
+Überprüft, ob ein Schiff an die angegeben Position gesetzt werden kann.  
+Die Position muss leer sein und es dürfen sich keine Schiffe in der *Nachbarschaft* befinden.  
+* `bool GetShipCount(bool[,] battlefield)`  
+  Zählt alle am Spielfeld platzierte Schiffe.  
+* `void SaveField(bool[,] battlefield, string fileName)`  
+  Das Spielfeld (gespeichert im `battlefield`) wird in eine Datei geschrieben. Die CSV Datei hat folgende Spalten: `"No;Row;Col"` (*No* ist eine fortlaufende Nummer).  
+  Hinweis: es werden nur *Schiffe* in die Datei geschrieben, die leeren Position jedoch nicht.  
+* `int[,]? LoadGame(int boardSize, string fileName)`  
+  Ein Spiel wird aus der angegebenen Datei geladen. Das Format der Datei entspricht der CSV Datei der Methode `SaveGame`.  
+  Hinweis: Die Datei kann eine "ungültige" Definition enthalten (ungültige Positionen der Schiffe). In diesem Fall liefert die Methode `null` als Rückgabewert.  
+
+Testen Sie das Programm ausführlich.  
+
+### Bildschirmausgabe
+
+```text
+BattleShip
+=========
+Board size [10,15 or 20]: x
+Ungültige Eingabe
+Board size [10,15 or 20]: 19
+Ungültige Größe
+Board size [10,15 or 20]: 10
+"row,col" to set a ship, e.g. 5,7
+"!row,col" to clear a ship, e.g. !5,7
+"exit" to quit program
+"save filename" to save the field (and continue)
+"load filename" revert and load the field from a file.
+=>
+```
---
+++ 10_Games/Übungen/BattleShipV2/Solution/BattleShip/BattleShip.cs
@@ -0,0 +1,389 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung
+ *--------------------------------------------------------------
+ * Description: BattleShip
+ *--------------------------------------------------------------
+ */
+
+namespace BattleShip;
+
+using System;
+using System.IO;
+
+public static class BattleShip
+{
+    /// <summary>
+    /// Executes the BattleShip program
+    /// </summary>
+    public static void Run()
+    {
+        Console.WriteLine("BattleShip ");
+        Console.WriteLine("=========");
+
+        bool[,] battlefield = InitField();
+
+        DesignField(battlefield);
+    }
+
+    public static bool[,] InitField()
+    {
+        int  boardSize;
+        bool isOk;
+
+        do
+        {
+            boardSize = Tools.ReadNumber("Board size [10,15 or 20]: ");
+            isOk = Tools.Contains(new[] { 10, 15, 20 }, boardSize);
+            if (!isOk)
+            {
+                Console.WriteLine("Ungültige Größe");
+            }
+        } while (!isOk);
+
+        Board.Init(boardSize, boardSize, "BattleShip");
+
+        return InitBattleField(boardSize);
+    }
+
+    /// <summary>
+    /// Initialize the field.
+    /// Mark all positions as empty.
+    /// </summary>
+    /// <param name="boardSize"></param>
+    /// <returns>The created and initialized field.</returns>
+    public static bool[,] InitBattleField(int boardSize)
+    {
+        var field = new bool[boardSize, boardSize];
+        for (int row = 0; row < boardSize; row++)
+        {
+            for (int col = 0; col < boardSize; col++)
+            {
+                Board.SetText(row, col, "");
+            }
+        }
+
+        return field;
+    }
+
+    /// <summary>
+    /// Continue read user input and set stone
+    /// </summary>
+    /// <param name="battlefield"></param>
+    public static void DesignField(bool[,] battlefield)
+    {
+        Console.WriteLine("\"row,col\" to set a ship, e.g. 5,7");
+        Console.WriteLine("\"!row,col\" to clear a ship, e.g. !5,7");
+        Console.WriteLine("\"exit\" to quit program");
+        Console.WriteLine("\"save filename\" to save the field (and continue)");
+        Console.WriteLine("\"load filename\" revert and load the field from a file.");
+
+        bool isEnd = false;
+
+        do
+        {
+            int    row;
+            int    col;
+            string fileName;
+
+            switch (GetUserInput(battlefield, out row, out col, out fileName))
+            {
+                case 0: // set ship on position row/col
+                    if (!SetShip(battlefield, row, col))
+                    {
+                        Console.WriteLine("Cannot set ship at {row}:{col}");
+                    }
+
+                    break;
+
+                case 1: // clear ship on position row/col
+
+                    if (!ClearShip(battlefield, row, col))
+                    {
+                        Console.WriteLine("No ship at {row}:{col}");
+                    }
+
+                    break;
+                case 3: // store
+                    SaveField(battlefield, fileName);
+                    Console.WriteLine($"Field saved to '{fileName}'.");
+                    break;
+
+                case 2: // load
+                    var field = LoadField(battlefield.GetLength(0), fileName);
+                    if (field == null)
+                    {
+                        Console.WriteLine("Illegal field!");
+                        Console.WriteLine("Load failed.");
+                        isEnd = true;
+                    }
+                    else
+                    {
+                        battlefield = field;
+                        Console.WriteLine($"Field loaded from '{fileName}'.");
+                    }
+
+                    break;
+
+                case 4: // quit
+                    isEnd = true;
+                    break;
+            }
+        } while (!isEnd);
+    }
+
+    /// <summary>
+    /// Set a ship on the battlefield.
+    /// Row and col must be an empty position.
+    /// </summary>
+    /// <param name="battlefield">The battlefield</param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <returns>true, the ship can be set, false otherwise</returns>
+    public static bool SetShip(bool[,] battlefield, int row, int col)
+    {
+        if (!CanSetShip(battlefield, row, col))
+        {
+            return false;
+        }
+
+        battlefield[row, col] = true;
+        Board.SetText(row, col, "o");
+
+        return true;
+    }
+
+    /// <summary>
+    /// Check, if the ship can be set at the given position.
+    /// </summary>
+    /// <param name="battlefield"></param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <returns>true if ship can be set, otherwise false.</returns>
+    public static bool CanSetShip(bool[,] battlefield, int row, int col)
+    {
+        if (battlefield[row, col])
+        {
+            return false;
+        }
+
+        int minRow = Math.Max(0, row - 1);
+        int maxRow = Math.Min(battlefield.GetLength(0)-1, row + 1);
+
+        int minCol = Math.Max(0, col - 1);
+        int maxCol = Math.Min(battlefield.GetLength(1)-1, col + 1);
+
+        int count = 0;
+
+        for (int r = minRow; r <= maxRow; r++)
+        {
+            for (int c = minCol; c <= maxCol; c++)
+            {
+                if (battlefield[r, c])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count == 0;
+    }
+
+    /// <summary>
+    /// Clear a ship on the battlefield.
+    /// </summary>
+    /// <param name="battlefield">The battlefield</param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <returns>true, the ship can be cleared, false otherwise</returns>
+    public static bool ClearShip(bool[,] battlefield, int row, int col)
+    {
+        if (!battlefield[row, col])
+        {
+            return false;
+        }
+
+        battlefield[row, col] = false;
+        Board.SetText(row, col, "");
+
+        return true;
+    }
+
+    /// <summary>
+    /// Show menu and read user input.
+    /// Repeat until valid input.
+    /// </summary>
+    /// <param name="battlefield"></param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
+    /// <param name="filename"></param>
+    /// <returns>0 set ship, 1 clear ship, 2 load, 3 save, 4 exit</returns>
+    public static int GetUserInput(bool[,] battlefield, out int row, out int col, out string filename)
+    {
+        int  result;
+        bool isOk;
+        row      = col = 0;
+        filename = string.Empty;
+
+        int rows = battlefield.GetLength(0);
+        int cols = battlefield.GetLength(1);
+
+        do
+        {
+            isOk = true;
+
+            Console.Write("=> ");
+            string input = (Console.ReadLine() ?? "").Trim();
+
+            if (input == "exit")
+            {
+                result = 4;
+            }
+            else if (input.StartsWith("load "))
+            {
+                result   = 2;
+                filename = input.Substring("load".Length).Trim();
+                isOk     = !string.IsNullOrEmpty(filename);
+            }
+            else if (input.StartsWith("save "))
+            {
+                result   = 3;
+                filename = input.Substring("save".Length).Trim();
+                isOk     = !string.IsNullOrEmpty(filename);
+            }
+            else
+            {
+                if (input.StartsWith("!"))
+                {
+                    result = 1;
+                    input  = input.Substring(1).Trim();
+                }
+                else
+                {
+                    result = 0;
+                }
+
+                string[] rowCol = input.Split(',');
+
+                isOk = rowCol.Length == 2 &&
+                       Tools.TryParse(rowCol[0], out row, 0, rows - 1) &&
+                       Tools.TryParse(rowCol[1], out col, 0, cols - 1);
+            }
+
+            if (!isOk)
+            {
+                Console.WriteLine("Illegal input, please try again");
+            }
+        } while (!isOk);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Count all ships on the battlefield.
+    /// </summary>
+    /// <param name="battlefield"></param>
+    /// <returns>Count of stones on the battlefield</returns>
+    public static int GetShipCount(bool[,] battlefield)
+    {
+        int rows = battlefield.GetLength(0);
+        int cols = battlefield.GetLength(1);
+
+        int count = 0;
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                if (battlefield[row, col])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /// <summary>
+    /// Save the current field to the specified file (as excel csv).
+    /// </summary>
+    /// <param name="battlefield">the definition for the current field.</param>
+    /// <param name="fileName">The destination filename.</param>
+    public static void SaveField(bool[,] battlefield, string fileName)
+    {
+        var lines = new string[1 + GetShipCount(battlefield)]; // +1 because of header
+
+        int rows  = battlefield.GetLength(0);
+        int cols  = battlefield.GetLength(1);
+        int count = 0;
+
+        lines[count++] = "No;Row;Col;Player";
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                if (battlefield[row, col])
+                {
+                    lines[count] = $"{count};{row};{col}";
+                    count++;
+                }
+            }
+        }
+
+        if (File.Exists(fileName))
+        {
+            string bakFileName = $"{Path.GetFileNameWithoutExtension(fileName)}.bak";
+
+            if (File.Exists(bakFileName))
+            {
+                File.Delete(bakFileName);
+            }
+            File.Move(fileName, bakFileName);
+        }
+
+        File.WriteAllLines(fileName, lines);
+    }
+
+    /// <summary>
+    /// Load a stored field from a file (csv format).
+    /// The file must be in a correct format (no validation check)
+    /// and the boardSize must match.
+    /// Errors are ignored
+    /// </summary>
+    /// <param name="boardSize">10,15 or 20</param>
+    /// <param name="fileName">Source file-name.</param>
+    /// <returns></returns>
+    public static bool[,]? LoadField(int boardSize, string fileName)
+    {
+        var lines = File.ReadAllLines(fileName);
+        var field = InitBattleField(boardSize);
+
+        if (lines.Length > 0)
+        {
+            bool first = true;
+
+            foreach (var line in lines)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    var part = line.Split(';');
+                    int row  = int.Parse(part[1]);
+                    int col  = int.Parse(part[2]);
+
+                    if (!SetShip(field, row, col))
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+
+        return field;
+    }
+}
