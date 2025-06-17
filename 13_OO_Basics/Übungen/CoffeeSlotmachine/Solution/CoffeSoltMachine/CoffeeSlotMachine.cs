1972a9553b7bf687c02f9e8d9de8ce0989df50be
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2025-06-17 15:54:07 +0200

    CoffeeSlotMachine

--- 13_OO_Basics/Übungen/CoffeeSlotmachine/Solution/CoffeSoltMachine/CoffeeSlotMachine.cs
+++ 13_OO_Basics/Übungen/CoffeeSlotmachine/Solution/CoffeSoltMachine/CoffeeSlotMachine.cs
@@ -1,155 +1,154 @@
 ﻿/*--------------------------------------------------------------
-*				HTBLA-Leonding / Class: 1xHIF
-*--------------------------------------------------------------
-*              Musterlösung-HA
-*--------------------------------------------------------------
-* Description: CoffeeSlotMachine
-*--------------------------------------------------------------
-*/
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: CoffeeSlotMachine
+ *--------------------------------------------------------------
+ */
 
-namespace CoffeeSlotMachine
+namespace CoffeeSlotMachine;
+
+using System;
+
+public class CoffeeSlotMachine
 {
-    using System;
-
-    public class CoffeeSlotMachine
+    public int CoinsInDepot
     {
-        public int CoinsInDepot
+        get { return CommonTools.Sum(_coinsAmount); }
+    }
+
+    public int Credit { get; private set; }
+
+    public int ProductsAvailable { get; private set; }
+
+    const int PriceOfProducts = 50;
+
+    string[] _products = [];
+
+    int[] _acceptedCoinValues = { 5, 10, 20, 50, 100, 200 };
+    int[] _coinsAmount        = [];
+
+    int[] _boughtProducts;
+
+    int[] _insertedCoins = [];
+
+    public CoffeeSlotMachine() : this(new[] { 3, 3, 3, 3, 3, 3 }, new[] { "Cappuccino", "Mocca", "Kakao" })
+    {
+    }
+
+    public CoffeeSlotMachine(int[] initialCoinsAmount, string[] products)
+    {
+        _coinsAmount = (int[]) initialCoinsAmount.Clone();
+
+        _products         = (string[]) products.Clone();
+        ProductsAvailable = _products.Length;
+        _boughtProducts   = new int[_products.Length];
+
+        CancelOrder();
+    }
+
+    public bool InsertCoin(int coin)
+    {
+        var index = CommonTools.IndexOf(_acceptedCoinValues, coin);
+
+        if (index < 0 || Credit >= PriceOfProducts)
         {
-            get { return CommonTools.Sum(_coinsAmount); }
+            return false;
         }
 
-        public int Credit { get; private set; }
+        _insertedCoins[index]++;
+        Credit += coin;
+        return true;
+    }
 
-        public int ProductsAvailable { get; private set; }
+    public bool SelectProduct(string product, out int[] returnCoins, out int donation)
+    {
+        returnCoins = new int[_acceptedCoinValues.Length];
 
-        const int PriceOfProducts = 50;
+        int productIdx = CommonTools.IndexOf(_products, product);
 
-        string[] _products;
-
-        int[] _acceptedCoinValues = { 5, 10, 20, 50, 100, 200 };
-        int[] _coinsAmount;
-
-        int[] _boughtProducts;
-
-        int[] _insertedCoins;
-
-        public CoffeeSlotMachine() : this(new[] { 3, 3, 3, 3, 3, 3 }, new[] { "Cappuccino", "Mocca", "Kakao" })
+        if (productIdx < 0 || Credit < PriceOfProducts)
         {
+            donation = 0;
+            return false;
         }
 
-        public CoffeeSlotMachine(int[] initialCoinsAmount, string[] products)
+        _boughtProducts[productIdx]++;
+
+        for (int i = 0; i < _insertedCoins.Length; i++)
         {
-            _coinsAmount = initialCoinsAmount.Clone() as int[];
-
-            _products         = products.Clone() as string[];
-            ProductsAvailable = _products.Length;
-            _boughtProducts   = new int[_products.Length];
-
-            CancelOrder();
+            _coinsAmount[i] += _insertedCoins[i];
         }
 
-        public bool InsertCoin(int coin)
-        {
-            var index = CommonTools.IndexOf(_acceptedCoinValues, coin);
+        donation = Credit - PriceOfProducts;
 
-            if (index < 0 || Credit >= PriceOfProducts)
+        for (int coinIndex = _acceptedCoinValues.Length - 1; coinIndex >= 0; coinIndex--)
+        {
+            var needCoinsOfAmount = donation / _acceptedCoinValues[coinIndex];
+            var countOfAmount     = Math.Min(needCoinsOfAmount, _coinsAmount[coinIndex]);
+
+            if (countOfAmount > 0)
             {
-                return false;
+                returnCoins[coinIndex]  =  countOfAmount;
+                _coinsAmount[coinIndex] -= countOfAmount;
+                donation                -= _acceptedCoinValues[coinIndex] * countOfAmount;
             }
-
-            _insertedCoins[index]++;
-            Credit += coin;
-            return true;
         }
 
-        public bool SelectProduct(string product, out int[] returnCoins, out int donation)
+        CancelOrder();
+
+        return true;
+    }
+
+    public int[] CancelOrder()
+    {
+        var order = _insertedCoins;
+
+        _insertedCoins = new int[_acceptedCoinValues.Length];
+        Credit         = 0;
+
+        return order;
+    }
+
+    public int EmptyDepot()
+    {
+        int cents = 0;
+
+        for (int i = 0; i < _coinsAmount.Length; i++)
         {
-            returnCoins = new int[_acceptedCoinValues.Length];
-
-            int productIdx = CommonTools.IndexOf(_products, product);
-
-            if (productIdx < 0 || Credit < PriceOfProducts)
-            {
-                donation = 0;
-                return false;
-            }
-
-            _boughtProducts[productIdx]++;
-
-            for (int i = 0; i < _insertedCoins.Length; i++)
-            {
-                _coinsAmount[i] += _insertedCoins[i];
-            }
-
-            donation = Credit - PriceOfProducts;
-
-            for (int coinIndex = _acceptedCoinValues.Length - 1; coinIndex >= 0; coinIndex--)
-            {
-                var needCoinsOfAmount = donation / _acceptedCoinValues[coinIndex];
-                var countOfAmount     = Math.Min(needCoinsOfAmount, _coinsAmount[coinIndex]);
-
-                if (countOfAmount > 0)
-                {
-                    returnCoins[coinIndex]  =  countOfAmount;
-                    _coinsAmount[coinIndex] -= countOfAmount;
-                    donation                -= _acceptedCoinValues[coinIndex] * countOfAmount;
-                }
-            }
-
-            CancelOrder();
-
-            return true;
+            cents           += _coinsAmount[i] * _acceptedCoinValues[i];
+            _coinsAmount[i] =  0;
         }
 
-        public int[] CancelOrder()
+        return cents;
+    }
+
+    public bool GetCounterForProduct(string product, out int counter)
+    {
+        int productIdx = CommonTools.IndexOf(_products, product);
+
+        if (productIdx < 0)
         {
-            var order = _insertedCoins;
-
-            _insertedCoins = new int[_acceptedCoinValues.Length];
-            Credit         = 0;
-
-            return order;
+            counter = 0;
+            return false;
         }
 
-        public int EmptyDepot()
+        counter = _boughtProducts[productIdx];
+        return true;
+    }
+
+    public bool GetCounterForCoin(int coin, out int counter)
+    {
+        int index = CommonTools.IndexOf(_acceptedCoinValues, coin);
+
+        if (index < 0)
         {
-            int cents = 0;
-
-            for (int i = 0; i < _coinsAmount.Length; i++)
-            {
-                cents           += _coinsAmount[i] * _acceptedCoinValues[i];
-                _coinsAmount[i] =  0;
-            }
-
-            return cents;
+            counter = 0;
+            return false;
         }
 
-        public bool GetCounterForProduct(string product, out int counter)
-        {
-            int productIdx = CommonTools.IndexOf(_products, product);
-
-            if (productIdx < 0)
-            {
-                counter = 0;
-                return false;
-            }
-
-            counter = _boughtProducts[productIdx];
-            return true;
-        }
-
-        public bool GetCounterForCoin(int coin, out int counter)
-        {
-            int index = CommonTools.IndexOf(_acceptedCoinValues, coin);
-
-            if (index < 0)
-            {
-                counter = 0;
-                return false;
-            }
-
-            counter = _coinsAmount[index];
-            return true;
-        }
+        counter = _coinsAmount[index];
+        return true;
     }
 }
--- 13_OO_Basics/Übungen/CoffeeSlotmachine/Template/CoffeSoltMachine/CoffeeSlotMachine.cs
+++ 13_OO_Basics/Übungen/CoffeeSlotmachine/Template/CoffeSoltMachine/CoffeeSlotMachine.cs
@@ -1,22 +1,21 @@
 ﻿/*--------------------------------------------------------------
-*				HTBLA-Leonding / Class: 1xHIF
-*--------------------------------------------------------------
-*              Musterlösung-HA
-*--------------------------------------------------------------
-* Description: CoffeeSlotMachine
-*--------------------------------------------------------------
-*/
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: CoffeeSlotMachine
+ *--------------------------------------------------------------
+ */
 
-namespace CoffeeSlotMachine
+namespace CoffeeSlotMachine;
+
+public class CoffeeSlotMachine
 {
-    public class CoffeeSlotMachine
-    {
-        //TODO: Implement CoffeeSlot Object here
-        //TODO: Constructors
-        //TODO: private members
-        //TODO: Properties
-        //TODO: public Methods
-        //TODO: private Methods
-        //TODO: please use same name, parameter and return types as used in the unit-tests - you must not change the unit-tests.
-    }
+    //TODO: Implement CoffeeSlot Object here
+    //TODO: Constructors
+    //TODO: private members
+    //TODO: Properties
+    //TODO: public Methods
+    //TODO: private Methods
+    //TODO: please use same name, parameter and return types as used in the unit-tests - you must not change the unit-tests.
 }
