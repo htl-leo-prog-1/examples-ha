01279469d15165d3b5595d4c2847ec976720461d
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2026-03-10 09:36:53 +0100

    sprechstunden

--- 13-Datenkapsel/Sprechstundenliste/Solution/ConsultingHours/RoomStatistic.cs
+++ 13-Datenkapsel/Sprechstundenliste/Solution/ConsultingHours/RoomStatistic.cs
@@ -4,31 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsultingHours
+namespace ConsultingHours;
+
+public class RoomStatistic
 {
-    public class RoomStatistic
+    private string _room="";
+    private int _count;
+
+    public void SetRoom(string room)
     {
-        private string _room;
-        private int _count;
+        _room = room;
+    }
 
-        public void SetRoom(string room)
-        {
-            _room = room;
-        }
+    public string GetRoom()
+    {
+        return _room;
+    }
 
-        public string GetRoom()
-        {
-            return _room;
-        }
+    public void SetCount(int count)
+    {
+        _count = count;
+    }
 
-        public void SetCount(int count)
-        {
-            _count = count;
-        }
-
-        public int GetCount()
-        {
-            return _count;
-        }
+    public int GetCount()
+    {
+        return _count;
     }
 }
--- 13-Datenkapsel/Sprechstundenliste/Solution/ConsultingHours/Teacher.cs
+++ 13-Datenkapsel/Sprechstundenliste/Solution/ConsultingHours/Teacher.cs
@@ -1,65 +1,53 @@
 ﻿using System;
 
-namespace ConsultingHours
+namespace ConsultingHours;
+
+public class Teacher
 {
-    public class Teacher
+    private string _name = "";
+    private string _day = "";
+    private string _time = "";
+    private string _room = "";
+
+    public void SetName(string name)
     {
-        private string _title;
-        private string _name;
-        private string _day;
-        private string _time;
-        private string _room;
-
-        public void SetTitle(string title)
-        {
-            _title = title;
-        }
-
-        public string GetTitle()
-        {
-            return _title;
-        }
-
-        public void SetName(string name)
-        {
-            _name = name;
-        }
-
-        public string GetName()
-        {
-            return _name;
-        }
-
-        public void SetDay(string day)
-        {
-            _day = day;
-        }
-
-        public string GetDay()
-        {
-            return _day;
-        }
-
-
-        public void SetTime(string time)
-        {
-            _time = time;
-        }
-
-        public string GetTime()
-        {
-            return _time;
-        }
-
-        public void SetRoom(string room)
-        {
-            _room = room;
-        }
-
-        public string GetRoom()
-        {
-            return _room;
-        }
-
+        _name = name;
     }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetDay(string day)
+    {
+        _day = day;
+    }
+
+    public string GetDay()
+    {
+        return _day;
+    }
+
+
+    public void SetTime(string time)
+    {
+        _time = time;
+    }
+
+    public string GetTime()
+    {
+        return _time;
+    }
+
+    public void SetRoom(string room)
+    {
+        _room = room;
+    }
+
+    public string GetRoom()
+    {
+        return _room;
+    }
+
 }
